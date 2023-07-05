0071482f9978edc0ecde02ac5194259764cbb980
Johban Clavijo <johbanc@gmail.com>
2023-07-04 23:29:38 -0500

    en el error del DEV

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,36 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::controller(PageController::class)->group(function () {
-
-  Route::get('/', 'home')->name('home');
-  Route::get('blog', 'blog')->name('blog');
-  Route::get('blog/{post:slug}', 'post')->name('post');
+    Route::get('/', 'home')->name('home');
+    Route::get('blog', 'blog')->name('blog');
+    Route::get('blog/{post:slug}', 'post')->name('post');
 });
 
+
 Route::get('/dashboard', function () {
-  return view('dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 /*
 Route::middleware('auth')->group(function () {
-  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 */
 require __DIR__ . '/auth.php';
