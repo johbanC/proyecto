4e3f59468ff679741cf4dd8813f06e895b39c8e4
Johban Clavijo <johbanc@gmail.com>
2023-09-13 22:59:05 -0500

    Guardado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- el que es @vite no funciona toca colocarlo asi directamente -->
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
