c93195f8e5fcf245b5e49932574dbb92103c94bb
Johban Clavijo <johbanc@gmail.com>
2023-09-25 23:19:39 -0500

    Editar, Eliminar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,27 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+
+    public function index()
+    {
+
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
+    }
+
+    public function destroy(Post $post)
+    {
+
+        $post->delete();
+
+        return back();
+    }
 }
